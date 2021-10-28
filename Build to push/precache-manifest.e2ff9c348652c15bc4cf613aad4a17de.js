28d50978a88c1e0bd15d8e95e51602de6b7208ff
hamza.mahmood@funavry.com-at-890242958078 <hamza.mahmood@funavry.com-at-890242958078>
2021-10-28 21:40:09 +0500

    Build

---
+++ Build to push/precache-manifest.e2ff9c348652c15bc4cf613aad4a17de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1c64f639398be86d986e34ae32a64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac93a4e8c20a35a1cdc6",
+    "url": "/static/css/main.ad4494ce.chunk.css"
+  },
+  {
+    "revision": "96a8611fa1dd336f5c7a",
+    "url": "/static/js/2.bdb7330d.chunk.js"
+  },
+  {
+    "revision": "4c0c83c372d3b6e92da93dcb6e2c075e",
+    "url": "/static/js/2.bdb7330d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac93a4e8c20a35a1cdc6",
+    "url": "/static/js/main.94aacdde.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
